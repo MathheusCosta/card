774d8563523b0abffe22798636b660096f252748
mathheuscosta <matheuscostamatheus62@hotmail.com>
2023-06-22 10:38:33 -0300

    algo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from "./components/Profile";
+
+export default function app() {
+  return (
+    <div className="app">
+      <Profile
+      avatar="https://avatars.githubusercontent.com/u/114931849?s=400&u=01f8bb6dc9c8110814348fe894b2ba0dfcd2cb74&v=4"
+      name="Matheus Costa"
+      bio=" Desenvolvedor Front-End"
+      email="matheuscostamatheus62@hotmail.com"
+      phone="(24)99273-4560"
+      githubUrl="https://github.com/MathheusCosta"
+      linkedinUrl="https://www.linkedin.com/in/mathheuscosta/"
+      instagramUrl="https://www.instagram.com/mathheuscostta/?igshid=NTE5MzUyOTU%3D"
+      />
+    </div>
+  )
+}
